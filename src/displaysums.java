2e8352379df999a74a896bfc230ffaaf382565af
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-07-05 10:20:57 -0400

    first and last commit

---
+++ src/displaysums.java
@@ -0,0 +1,10 @@
+public class displaysums {
+    public static void main(String[] args) {
+        int sum = 0;
+        int n = 1000;
+        for(int i = 1; i <= n ; i++){
+            sum+=i;
+        }
+        System.out.println("Sum = " +sum);
+    }
+}
